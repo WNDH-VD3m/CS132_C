fbef40d2e422a07a1cd1df01fa68e12e74686e0d
WNDH-VD3m <77713690+WNDH-VD3m@users.noreply.github.com>
2021-01-23 23:58:55 +0800

    File I/O

---
+++ test3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1024
+
+
+int main()
+{
+  FILE *fp;
+  char buffer[MAX];
+
+
+
+  if((fp = fopen("lines.txt", "w")) == NULL)
+  {
+    printf("Fail to open!\n");
+    exit(EXIT_FAILURE);
+  }
+
+
+  fputs("Hello world!\n" , fp);
+  fputs("test!!!\n", fp);
+  fputs("HAHAHAHA\0", fp);
+
+  fclose(fp);
+
+  if((fp = fopen("lines.txt", "r")) == NULL)
+  {
+    printf("Fail to open!\n");
+    exit(EXIT_FAILURE);
+  }
+  while (!feof(fp))
+  {
+    fgets(buffer, MAX , fp);
+    printf("%s", buffer);
+  }
+
+  return 0;
+
+
+
+}
